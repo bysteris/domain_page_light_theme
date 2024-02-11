e3a9b3d82975826c88a9886798279b2d34c158fc
Steris <bstepanov@gk-sneg.com>
2024-02-11 15:16:51 +0500

    gh pahe

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: `/domain_page — light_theme/`,
+  publicPath: `/domain_page_light_theme/`,
   transpileDependencies: true
 })
